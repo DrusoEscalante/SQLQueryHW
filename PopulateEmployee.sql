f37b9e8b13ff6d398900aa04f8514578ba7c6b15
DrusoEscalante <135192552+DrusoEscalante@users.noreply.github.com>
2025-03-19 13:22:42 -0700

    Delete PopulateEmployee.sql

--- PopulateEmployee.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO Employee(EmployeeId,EmployeeName)VALUES(211,'Druso')
-INSERT INTO Employee(EmployeeId,EmployeeName)VALUES(321,'Kobe')
-INSERT INTO Employee(EmployeeId,EmployeeName)VALUES(754,'Bob')
